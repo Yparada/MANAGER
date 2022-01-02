fc654367f7263440499b2289e230367de4e0b13a
yparada <yucethpt@gmail.com>
2022-01-02 00:17:03 -0500

    add health operation

---
+++ src/test/java/com/ytarama/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytarama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
